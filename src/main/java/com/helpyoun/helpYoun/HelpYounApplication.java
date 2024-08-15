b200df8af7372cd6af539374885c96c8fca8f309
milk-stone <ywshero@naver.com>
2024-08-15 14:08:55 +0900

    init : Spring 초기 설정

---
+++ src/main/java/com/helpyoun/helpYoun/HelpYounApplication.java
@@ -0,0 +1,13 @@
+package com.helpyoun.helpYoun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpYounApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpYounApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpyoun/helpYoun/HelpYounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpyoun.helpYoun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpYounApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
