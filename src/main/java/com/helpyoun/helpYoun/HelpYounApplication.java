637c161883d1da16c92902a4cf5c4f1538e2eb1c
milk-stone <ywshero@naver.com>
2024-08-18 03:47:35 +0900

    feat : EnableJpaAuditing 어노테이션 추가 및 배포 파일 생성 완료

--- src/main/java/com/helpyoun/helpYoun/HelpYounApplication.java
+++ src/main/java/com/helpyoun/helpYoun/HelpYounApplication.java
@@ -2,8 +2,10 @@ package com.helpyoun.helpYoun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HelpYounApplication {
 
 	public static void main(String[] args) {
